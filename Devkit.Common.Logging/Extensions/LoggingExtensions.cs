5f42192851c1c3ddfb7a603e12823c6007907933
Hacı Coşkun <hacicoskun07@gmail.com>
2026-01-09 13:57:49 +0300

    Add custom Serilog-based logging module
    Introduced Devkit.Common.Logging project with Serilog integration, including extension methods and configuration options for Seq and Elasticsearch sinks. Updated solution and dependencies, added logging configuration to appsettings, and integrated logging into Devkit.Sample.Api.

---
+++ Devkit.Common.Logging/Extensions/LoggingExtensions.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using Serilog.Exceptions;
+using Serilog.Sinks.Elasticsearch;
+using Devkit.Common.Logging.Options;
+using Microsoft.Extensions.Logging;
+
+namespace Devkit.Common.Logging.Extensions;
+
+public static class LoggingExtensions
+{
+    public static IServiceCollection AddCustomLogging(this IServiceCollection services, IConfiguration configuration)
+    {
+        var options = configuration.GetSection("LoggingOptions").Get<LoggingOptions>();
+
+        var loggerConfiguration = new LoggerConfiguration()
+            .ReadFrom.Configuration(configuration)
+            .Enrich.FromLogContext()
+            .Enrich.WithProperty("ApplicationName", options!.ApplicationName)
+            .Enrich.WithExceptionDetails() 
+            .Enrich.WithClientIp() 
+            .Enrich.WithRequestHeader("User-Agent", "UserAgent") 
+            .Enrich.WithThreadId()
+            .Enrich.WithEnvironmentName()
+            .WriteTo.Console();
+
+        if (options.Seq is { IsEnabled: true })
+        {
+            loggerConfiguration.WriteTo.Seq(
+                serverUrl: options.Seq.ServerUrl,
+                apiKey: options.Seq.ApiKey);
+        }
+
+        if (options.Elastic is { IsEnabled: true })
+        {
+            loggerConfiguration.WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(options.Elastic.NodeUrl))
+            {
+                AutoRegisterTemplate = true,
+                IndexFormat = options.Elastic.IndexFormat ?? "devkit-logs-{0:yyyy.MM.dd}"
+            });
+        }
+
+        Log.Logger = loggerConfiguration.CreateLogger();
+
+        services.AddLogging(loggingBuilder =>
+        {
+            loggingBuilder.ClearProviders();
+            loggingBuilder.AddSerilog(Log.Logger);
+        });
+
+        return services;
+    }
+}
